773ad3510c729d270653e6ac5233a5d3b3a82312
ravibkjoshi <ravibkjoshi@gmail.com>
2017-04-17 15:05:32 -0700

    changed readme and message in messages controller

--- app/controllers/messages.rb
+++ app/controllers/messages.rb
@@ -8,7 +8,7 @@ post '/messages' do
   @user = current_user
   @selected_contacts = params[:numbers]
 
-  p "*" * 80
+  
   p @selected_contacts.class
   p params
   @client = Twilio::REST::Client.new ENV['account_sid'], ENV['auth_token']
@@ -21,8 +21,8 @@ post '/messages' do
 	  @client.messages.create(
 	  from: '+14083354139 ',
 	   to: internationalized_phone_number(contact),
-	  body: "#{params[:message]}"
+	  body: "#{params[:message]} From: EventText Client"
 )	end
- 
+
 end
 
